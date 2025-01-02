b43fbd5987bdb0db99537d149bd522afefb77865
Niklas Wojtkowiak <niklas@wojtkowiak.com>
2025-01-02 13:36:49 -0500

    fix root

--- MyGroupCommand.cs
+++ MyGroupCommand.cs
@@ -1,10 +1,11 @@
 ﻿namespace oEconomy;
 
 //All command modules are created with a scoped lifetime
+[RequirePermission(PermissionCheckType.Any, true, "eco.admin")]
 [CommandGroup("eco")]
-public class MyGroupCommand : CommandModuleBase
+public class EconomyCommand : CommandModuleBase
 {
-    [Inject] public ILogger<MyGroupCommand> Logger { get; set; }
+    [Inject] public ILogger<EconomyCommand> Logger { get; set; }
     [Inject] public VaultApi VaultApi { get; set; }
     
     [Command("give")]
@@ -14,7 +15,7 @@ public class MyGroupCommand : CommandModuleBase
         VaultApi.AddBalance(playerName, a);
         Logger.LogInformation("Added {a} to {playerName}", a, playerName);
     }
-    
+
     [Command("take")]
     [CommandInfo("Takes a players money.")]
     public async Task Take(string playerName, int a)
@@ -22,7 +23,7 @@ public class MyGroupCommand : CommandModuleBase
         VaultApi.DeductBalance(playerName, a);
         Logger.LogInformation("Took {a} from {playerName}", a, playerName);
     }
-    
+
     [Command("set")]
     [CommandInfo("Sets players money to a value.")]
     public async Task Set(string playerName, int a)
@@ -32,5 +33,30 @@ public class MyGroupCommand : CommandModuleBase
         VaultApi.AddBalance(playerName, a);
         Logger.LogInformation("Set {playerName} balance to {a}", playerName, a);
     }
+}
 
+[CommandGroup("balance", "bal")]
+public class BalanceCommands : CommandModuleBase
+{
+    [Inject] public ILogger<BalanceCommands> Logger { get; set; }
+    [Inject] public VaultApi VaultApi { get; set; }
+
+    [Command("")]
+    [CommandInfo("Gets the balance of a player.")]
+    public async Task Balance()
+    {
+        var playerName = this.CommandContext.Player.Username;
+        var balance = VaultApi.GetBalance(playerName);
+        //Logger.LogInformation("You have a balance of {balance}", balance);
+        await this.Player.SendMessageAsync($"You have a balance of {balance}");
+    }
+    
+    [Command("")]
+    [CommandOverload]
+    public async Task BalanceOther(string playerName)
+    {
+        var balance = VaultApi.GetBalance(playerName);
+        Logger.LogInformation("{playerName} has a balance of {balance}", playerName, balance);
+        await this.Player.SendMessageAsync($"{playerName} has a balance of {balance}");
+    }
 }
