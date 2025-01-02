c3564d9f928725a255f830c0cf4168c5178731ae
Niklas Wojtkowiak <niklas@wojtkowiak.com>
2025-01-02 10:50:54 -0500

    Init

---
+++ MyGroupCommand.cs
@@ -0,0 +1,24 @@
+﻿namespace oEconomy;
+
+//All command modules are created with a scoped lifetime
+[CommandGroup("mygroupCommand")]
+public class MyGroupCommand : CommandModuleBase
+{
+    [Inject] public ILogger<MyGroupCommand> Logger { get; set; }
+
+    [Command("subtract")]
+    [CommandInfo("woop dee doo this command is from a plugin")]
+    public async Task Subtract(int a, int b)
+    {
+        Logger.LogInformation("Subtracted {a}-{b}={c}", a, b, a - b);
+        await this.Player.SendMessageAsync($"Subtracted {a}-{b}={a - b}");
+    }
+
+    [Command("add")]
+    [CommandInfo("woop dee doo this command is from a plugin")]
+    public async Task Add(int a, int b)
+    {
+        Logger.LogInformation("Added {a}+{b}={c}", a, b, a + b);
+        await this.Player.SendMessageAsync($"Added {a}+{b}={a + b}");
+    }
+}
